60af6a145e5dee8569a4052c74a3e7e557b126f7
Amal Roy <amalroy2028@cs.sjcetpalai.ac.in>
2025-07-16 00:08:35 +0530

    First java program added

---
+++ BigNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class BigNumber {
+    
+
+
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int firstNumber,secondNumber,bigNumber;
+        System.out.println("Enter the first number:");
+        firstNumber=sc.nextInt();
+        System.out.println("Enter the second number:");
+        secondNumber=sc.nextInt();
+        bigNumber=firstNumber>secondNumber?firstNumber:secondNumber;
+    
+        System.out.println("I am "+" "+bigNumber+","+ "I am Big");
+
+
+    }
+
+
+}
