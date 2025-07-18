9549b422cf56e9c73118e954c75fe14b194c1ba9
Amal Roy <amalroy2028@cs.sjcetpalai.ac.in>
2025-07-18 22:45:11 +0530

    Added addTwoNumbers.java

---
+++ module_1/addTwoNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class addTwoNumbers {
+    
+
+
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int firstNumber,secondNumber,sum;
+        System.out.println("Enter the first number:");
+        firstNumber=sc.nextInt();
+        System.out.println("Enter the second number:");
+        secondNumber=sc.nextInt();
+        sum=firstNumber+secondNumber; 
+        System.out.println("The sum is" + " "+ sum);
+
+    }
+}
