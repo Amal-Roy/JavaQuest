5b73d81cb1b510a1d2d60b4433b030bf8f8a1f0f
Amal Roy <amalroy2028@cs.sjcetpalai.ac.in>
2025-07-18 23:04:35 +0530

    Added HelloWorld.java

---
+++ module_1/HelloWorld.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class HelloWorld {
+    
+    public static void main(String args[]){
+        System.out.println("HelloWorld!");
+    }
+}
+/* 
+author:Amal Roy
+Date:18-07-2025
+ */
