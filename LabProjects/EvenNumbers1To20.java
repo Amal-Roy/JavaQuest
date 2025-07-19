296580760a650331fdbfe8259122abdde41d9d62
Amal Roy <amalroy2028@cs.sjcetpalai.ac.in>
2025-07-19 10:51:28 +0530

    EvenNumbers1To20.java file is added

---
+++ LabProjects/EvenNumbers1To20.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class EvenNumbers1To20 {
+    public static void main(String[] args) {
+        int i=1;
+        System.out.println("The even numbers between 1 and 20 is:");
+    do{
+       if(i%2==0){
+         System.out.println(+i);
+       }
+       i++;
+    }while(i<=20);
+ }
+}
+
+/*
+ * Author:Amal Roy
+ * Date:19-07-2025
+ */
